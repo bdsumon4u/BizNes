51bb3822e0bb3b95f63e3b1022ca47b8ef0466d8
Sumon Ahmed <bdsumon4u@gmail.com>
2025-03-27 14:54:04 +0600

    feat: implement custom view and enhance CreatePurchase page with empty heading, subheading, and breadcrumbs methods for improved UI structure

--- app/Filament/Resources/PurchaseResource/Pages/CreatePurchase.php
+++ app/Filament/Resources/PurchaseResource/Pages/CreatePurchase.php
@@ -4,8 +4,26 @@ namespace App\Filament\Resources\PurchaseResource\Pages;
 
 use App\Filament\Resources\PurchaseResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class CreatePurchase extends CreateRecord
 {
     protected static string $resource = PurchaseResource::class;
+
+    protected static string $view = 'filament.pages.purchases.create';
+
+    public function getHeading(): string
+    {
+        return '';
+    }
+
+    public function getSubheading(): string|Htmlable|null
+    {
+        return '';
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        return [];
+    }
 }

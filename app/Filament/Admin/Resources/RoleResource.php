3c1844a86b6b6c7b52c66a55b4a71c5a94f0cea1
Sumon Ahmed <bdsumon4u@gmail.com>
2025-05-28 14:41:08 +0600

    wip

--- app/Filament/Admin/Resources/RoleResource.php
+++ app/Filament/Admin/Resources/RoleResource.php
@@ -230,9 +230,11 @@ class RoleResource extends Resource implements HasShieldPermissions
 
     public static function getNavigationBadge(): ?string
     {
-        return Utils::isResourceNavigationBadgeEnabled()
-            ? strval(static::getEloquentQuery()->count())
-            : null;
+        // return Utils::isResourceNavigationBadgeEnabled()
+        //     ? strval(static::getEloquentQuery()->count())
+        //     : null;
+
+        return null;
     }
 
     public static function isScopedToTenant(): bool
